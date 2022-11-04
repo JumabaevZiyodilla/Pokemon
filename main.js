02e8418c6fa15a7ecd0dee5c3f9240cce1d80ad0
Ziyodilla <ziyodillajz@gmail.com>
2022-11-04 11:23:48 +0500

    Done

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const elForm = document.querySelector(".pokemon-form");
 const elInput = document.querySelector(".pokemon-input");
 const elPokemonList = document.querySelector(".pokemon_list")
 const elTemplate = document.querySelector(".template").content;
+const elPokemonSelect = document.querySelector(".pokemon-select");
 const fragmentPokemon = document.createDocumentFragment();
+
+const elMinCandyCount = document.querySelector(".pokemon-min-year");
+const elMaxCandyCount = document.querySelector(".pokemon-max-year");
+
 
 function renderPokemon(pokemon) {
     elPokemonList.innerHTML = ""
@@ -18,16 +23,61 @@ function renderPokemon(pokemon) {
         pokemonFragmentClone.querySelector(".pokemon__span").textContent = elPoke.num;
         pokemonFragmentClone.querySelector(".pokemon__text").textContent = elPoke.type.join(", ");
         pokemonFragmentClone.querySelector(".pokemon__time").textContent = elPoke.spawn_time;
-
-
         fragmentPokemon.appendChild(pokemonFragmentClone);
-
     })
     elPokemonList.appendChild(fragmentPokemon);
-    
 }
-renderPokemon(pokemons)
 
+// SELECT WEAKNESS
+const pokemonType = [];
+const pokemonTypeValue = [];
+function selectValue(){
+    pokemons.forEach(item =>{
+        pokemonType.push(item.weaknesses);
+        pokemonType.forEach(element => {
+            element.forEach(item =>{
+                if(!pokemonTypeValue.includes(item)){
+                    pokemonTypeValue.push(item);
+                }
+            })
+        })
+        
+    })
+}
+
+
+
+selectValue();
+
+// CREATE SELECT ELEMENTS
+function selectCreate(){
+    const selectFragment = document.createDocumentFragment();
+    pokemonTypeValue.forEach(element => {
+        const elOption = document.createElement("option");
+        elOption.value = element
+        elOption.textContent = element
+        selectFragment.appendChild(elOption);
+    })
+    elPokemonSelect.appendChild(selectFragment);
+};
+
+selectCreate();
+
+// SELECT SORT
+const elSelectSort = document.querySelector(".pokemon-sort");
+
+function selectSort(filterArray,selectValue){
+    if(selectValue == "a-z"){
+        filterArray.sort((a,b) => a.name.charCodeAt(0) - b.name.charCodeAt(0));
+    }if(selectValue == "z-a"){
+        filterArray.sort((a,b) => b.name.charCodeAt(0) - a.name.charCodeAt(0))
+    }if(selectValue == "thin-thick"){
+        filterArray.sort((a,b) => a.weight.charCodeAt() - b.weight.charCodeAt());
+    }
+    if(selectValue == "thick-thin"){
+        filterArray.sort((a,b) => b.weight.charCodeAt(0) - a.weight.charCodeAt(0));
+    }
+}
 elForm.addEventListener("submit", (evt) =>{
     evt.preventDefault();
 
@@ -35,40 +85,13 @@ elForm.addEventListener("submit", (evt) =>{
     const regexInputValue = new RegExp(elInputValue, "gi");
     
     let searchPokemon = pokemons.filter(item => {
-        return item.name.match(regexInputValue);
+        return item.name.match(regexInputValue) && (elPokemonSelect.value == "All" || item.weaknesses.includes(elPokemonSelect.value)) && (elMinCandyCount.value == "" || item.candy_count >= elMinCandyCount.value) && (elMaxCandyCount.value == "" || item.candy_count <= elMaxCandyCount.value);
     })
     if(searchPokemon.length > 0){
+        selectSort(searchPokemon,elSelectSort.value)
         renderPokemon(searchPokemon);
     }else{
         elPokemonList.innerHTML = "Pokemon not found!"
     }
 })
-
-
-// var newPokemonItem = document.createElement("li");
-// var newPokemonTitle = document.createElement("h3");
-// var newPokemonImg = document.createElement("img");
-// var newPokemonSpan = document.createElement("span");
-// var newPokemonText = document.createElement("text");
-// var newPokemonTime = document.createElement("time");
-
-// newPokemonTitle.textContent = i.name;
-// newPokemonImg.src = i.img;
-// newPokemonSpan.textContent = i.num;
-// newPokemonText.textContent = i.type.join(" ");
-// newPokemonTime.textContent = i.spawn_time;
-
-// newPokemonItem.classList.add("pokemon__item");
-// newPokemonTitle.classList.add("pokemon__title")
-// newPokemonImg.width = "200";
-// newPokemonImg.height = "200";
-// newPokemonSpan.classList.add("pokemon__span");
-// newPokemonText.classList.add("pokemon__text");
-// newPokemonTime.classList.add("pokemon__time");
-
-// newPokemonItem.appendChild(newPokemonTitle);
-// newPokemonItem.appendChild(newPokemonImg);
-// newPokemonItem.appendChild(newPokemonSpan);
-// newPokemonItem.appendChild(newPokemonTime);
-// newPokemonItem.appendChild(newPokemonText);
-// pokemonList.appendChild(newPokemonItem);
+renderPokemon(pokemons)
